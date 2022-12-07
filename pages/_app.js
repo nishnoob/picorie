4f6860697856b6671ff03346a674d2b4e77b96e4
Agnish Mukherjee <agnish15m@gmail.com>
2022-12-07 16:32:54 +0530

    Updated prod env and added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,21 @@ import { UserProvider } from '@auth0/nextjs-auth0';
 
 import '../public/global.css';
 import Airtable from 'airtable';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   // Airtable.configure({ apiKey: process.env.AUTH0_CLIENT_ID });
   Airtable.configure({ apiKey: 'keyliL9lqi7Rgn8MT' });
 
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <>
+      <Head>
+        <title>picorie | albums for web </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </>
   );
 }
