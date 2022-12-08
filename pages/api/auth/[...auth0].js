d22c20f6ea33ddf97b5d4dd79b1f54fc78cff555
Agnish Mukherjee <agnish15m@gmail.com>
2022-12-08 21:31:27 +0530

    Minor

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,4 +1,12 @@
-// pages/api/auth/[...auth0].js
-import { handleAuth } from '@auth0/nextjs-auth0';
+import auth0 from 'src/util/auth0'
 
-export default handleAuth();
+export default auth0.handleAuth({
+  // other stuff here...
+  async callback(req, res) {
+    try {
+      await auth0.handleCallback(req, res)
+    } catch (error) {
+      res.redirect('/')
+    }
+  }
+})
